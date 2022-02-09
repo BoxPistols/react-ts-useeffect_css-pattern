e6759119010db36552702d9f22b3f7eed43eb60d
BoxPistols <ito.atsu.mail@gmail.com>
2022-02-10 04:00:33 +0900

    2 files modified

--- Hello.tsx
+++ Hello.tsx
@@ -6,15 +6,40 @@ const colors = {
   Sand: '#fceeb5',
   Peach: '#ee786e',
 };
+
 const texts = {
   s: '14px',
   m: '18px',
   l: '24px',
 };
 
-const Hello = ({ name }) => {
+const flexJcStyles = {
+  null: 'null',
+  start: 'flex-start',
+  center: 'center',
+  end: 'flex-end',
+};
+
+const alignItems = {
+  start: 'flex-start',
+  center: 'center',
+  end: 'flex-end',
+};
+
+export default ({ name }) => {
   const [color, setColor] = useState(colors.Sea);
   const [text, setText] = useState(texts.s);
+  const [open, setOpen] = useState(false);
+  const [flexJc, setFlexJc] = useState(flexJcStyles.start);
+
+  const handleClick = () => {
+    setOpen(!open);
+  };
+
+  useEffect(() => {
+    document.querySelector<HTMLInputElement>('.boxes').style.justifyContent =
+      flexJc;
+  }, [flexJc]);
 
   useEffect(() => {
     document.body.style.background = color;
@@ -26,20 +51,49 @@ const Hello = ({ name }) => {
 
   return (
     <Fragment>
+      <button onClick={handleClick}>boxNumChange</button>
+
+      {/* FlexBox */}
+      <select value={flexJc} onChange={(e) => setFlexJc(e.target.value)}>
+        {Object.entries(flexJcStyles).map(([name, value]) => (
+          <option key={`flexJc--${name}`} value={value}>
+            {name}
+          </option>
+        ))}
+      </select>
+
+      {open ? (
+        <div className="boxes" style={{ display: 'flex' }}>
+          <div className="flexBox">flexBox 1</div>
+          <div className="flexBox">flexBox 2</div>
+          <div className="flexBox">flexBox 3</div>
+        </div>
+      ) : (
+        <div className="boxes alot" style={{ display: 'flex' }}>
+          <div className="flexBox">flexBox 1</div>
+          <div className="flexBox">flexBox 2</div>
+          <div className="flexBox">flexBox 3</div>
+          <div className="flexBox">flexBox 4</div>
+          <div className="flexBox">flexBox 5</div>
+          <div className="flexBox">flexBox 6</div>
+          <div className="flexBox">flexBox 7</div>
+        </div>
+      )}
+
       <h1>Hello {name}!</h1>
 
       <select value={color} onChange={(e) => setColor(e.target.value)}>
-        {Object.entries(colors).map(([design, value]) => (
-          <option key={`color--${design}`} value={value}>
-            {design}
+        {Object.entries(colors).map(([name, value]) => (
+          <option key={`color--${name}`} value={value}>
+            {name}
           </option>
         ))}
       </select>
 
       <select value={text} onChange={(e) => setText(e.target.value)}>
-        {Object.entries(texts).map(([n, value]) => (
-          <option key={`text--${n}`} value={value}>
-            {n}
+        {Object.entries(texts).map(([name, value]) => (
+          <option key={`text--${name}`} value={value}>
+            {name}
           </option>
         ))}
       </select>
@@ -49,5 +103,3 @@ const Hello = ({ name }) => {
     </Fragment>
   );
 };
-
-export default Hello;
